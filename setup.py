b25d0f956ac9eab8fca22add0ef53154beaea0c4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2024-11-22 10:50:03 +0000

    Updated the build files

--- setup.py
+++ setup.py
@@ -2,10 +2,12 @@ import os
 import subprocess
 import sys
 from pathlib import Path
-import glob
+import pybind11
 
 from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
+
+import amulet_test1
 
 
 # https://github.com/pybind/cmake_example/blob/master/setup.py
@@ -20,9 +22,21 @@ class CMakeBuild(build_ext):
         ext_fullpath = Path.cwd() / self.get_ext_fullpath("")
         src_dir = ext_fullpath.parent.resolve()
 
+        platform_args = []
+        if sys.platform == "win32":
+            platform_args.extend(["-G", "Visual Studio 17 2022"])
+            if sys.maxsize > 2 ** 32:
+                platform_args.extend(["-A", "x64"])
+            else:
+                platform_args.extend(["-A", "Win32"])
+            platform_args.extend(["-T", "v143"])
+
         subprocess.run([
             "cmake",
+            *platform_args,
             f"-DPYTHON_EXECUTABLE={sys.executable}",
+            f"-Dpybind11_DIR={pybind11.get_cmake_dir().replace(os.sep, '/')}",
+            f'-Damulet_test1_DIR={amulet_test1.__path__[0]}',
             f"-DCMAKE_INSTALL_PREFIX=install",
             f'-DSRC_INSTALL_DIR={src_dir}',
             '-B', 'build',
@@ -30,10 +44,7 @@ class CMakeBuild(build_ext):
         subprocess.run(["cmake", "--build", "build", "--config", "Release"])
         subprocess.run(["cmake", "--install", "build", "--config", "Release"])
 
-        # Remove compile-time files
-        for ext in ["*.lib"]:
-            for path in glob.glob(os.path.join(glob.escape(src_dir), "**", ext), recursive=True):
-                os.remove(path)
+        # TODO: Add .0 to compiled dependency versions
 
 
 setup(
